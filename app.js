a4b1c376346c05304e46573f70540dac05cbca8b
Basant Jaiswal <bjaiswal@mahle.com>
2024-03-27 00:47:19 +0530

    backup commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const edubtn = document.getElementsByClassName(".edu-button")
+const expbtn = document.getElementsByClassName(".exp-button")
+const volbtn = document.getElementsByClassName(".vol-button")
+const edumenu = document.getElementsByClassName(".education-menu")
+const expmenu = document.getElementsByClassName(".experience-menu")
+const volmenu = document.getElementsByClassName(".volunteer-menu")
+
+edubtn.addEventListener("click", ()=> {
+    edumenu.
+} )
